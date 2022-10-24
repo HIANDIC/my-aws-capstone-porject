26031d8b4e32cd04409a2a735b10fcf203323470
HIANDIC <leaands25@gmail.com>
2022-10-24 11:48:30 +0200

    user data modified

--- userdata.sh
+++ userdata.sh
@@ -4,12 +4,12 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+git clone https://$TOKEN@github.com/HIANDIC/my-aws-capstone-porject.git
+cd /home/ubuntu/my-aws-capstone-porject
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/my-aws-capstone-porject/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
